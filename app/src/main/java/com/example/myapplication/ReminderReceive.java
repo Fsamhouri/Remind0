3a8215d5b1d97f6091412c342fbeec086b4eb66e
georget3 <georget3@illinois.edu>
2020-05-02 16:31:45 -0500

    built da app

--- app/src/main/java/com/example/myapplication/ReminderReceive.java
+++ app/src/main/java/com/example/myapplication/ReminderReceive.java
@@ -1,4 +1,28 @@
 package com.example.myapplication;
 
-public class ReminderReceive {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReminderReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int remindId = intent.getIntExtra("Reminder Value", 0);
+        String reminder = intent.getStringExtra("Task");
+        Intent main = new Intent(context, MainActivity.class);
+        PendingIntent pending = PendingIntent.getActivity(context, 0, main, 0);
+        NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("Remind0").setContentText(reminder)
+                .setWhen(System.currentTimeMillis()).setAutoCancel(true)
+                .setContentIntent(pending);
+        notifManager.notify(remindId, builder.build());
+
+    }
 }
